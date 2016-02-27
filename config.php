3f5c5ed6d68416f60d044a03dd8b9c8f4baa0c48
Milosch Meriac <milosch@meriac.com>
2016-02-27 19:29:45 +0000

    added license headers

--- config.php
+++ config.php
@@ -1,5 +1,28 @@
 <?php
+/***************************************************************
+ *
+ * OpenBeacon.org - Logfile Analysis Tools
+ *
+ * Copyright 2015 Milosch Meriac <milosch@meriac.com>
+ *
+ ***************************************************************
 
+ This file is part of the get.OpenBeacon.org logfile analysis tools
+
+ OpenBeacon is free software: you can redistribute it and/or modify
+ it under the terms of the GNU General Public License as published by
+ the Free Software Foundation, either version 3 of the License, or
+ (at your option) any later version.
+
+ OpenBeacon is distributed in the hope that it will be useful,
+ but WITHOUT ANY WARRANTY; without even the implied warranty of
+ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ GNU General Public License for more details.
+
+ You should have received a copy of the GNU General Public License
+ along with OpenBeacon.  If not, see <http://www.gnu.org/licenses/>.
+
+*/
 define('FILE_NAME', 'ward.log.bz2');
 define('TAG_TIMEOUT', 30);
 define('TIMEZONE', 'GMT');
